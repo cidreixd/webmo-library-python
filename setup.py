086643579fa06a1f9f7441ce2f589aef51ea199e
Cidre Interaction Design Inc <info@cidre.tokyo>
2016-10-11 03:45:46 +0900

    edit setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PyWebmo',
+  packages = ['PyWebmo'],
+  version = '0.1',
+  description = 'Webmo client library for Python',
+  author = 'Cidre Interaction Design Inc.',
+  author_email = 'info@cidre.tokyo',
+  url = 'https://github.com/cidreixd/webmo-library-python',
+  download_url = 'https://github.com/cidreixd/webmo-library-python/tarball/0.1',
+  keywords = ['webmo', 'websocket'], # arbitrary keywords
+  classifiers = [],
+)
